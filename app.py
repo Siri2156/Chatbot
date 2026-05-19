803c832973b6eff69b4d542c30257e3629ec9ae9
Siri Tula <sridhanya.tula@gmail.com>
2026-05-19 13:01:23 +0530

    initial commit

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ load_dotenv()
 DB_HOST = os.getenv("DB_HOST", "localhost")
 DB_USER = os.getenv("DB_USER", "root")
 DB_PASSWORD = os.getenv("DB_PASSWORD", "")
-DB_PORT = int(os.getenv("DB_PORT", 3306))
+DB_PORT = int(os.getenv("DB_PORT") or 3306)
 DB_NAME = os.getenv("DB_NAME", "quickgpt")
 
 # Create client
@@ -105,7 +105,7 @@ def setup_database():
             host=DB_HOST,
             user=DB_USER,
             password=DB_PASSWORD,
-            port=3306
+            port=DB_PORT
         )
 
         print("MySQL connected successfully")
